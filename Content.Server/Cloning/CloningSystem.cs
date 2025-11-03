c6d90727033c6764045f26ba09331b90fa019ec2
ScarKy0 <106310278+ScarKy0@users.noreply.github.com>
2025-11-03 14:05:56 +0100

    Clone toolshed commands (#41261)
    * init
    
    * fuck this file its such a mess why wont anyone sort this holy shit
    
    * review

--- Content.Server/Cloning/CloningSystem.cs
+++ Content.Server/Cloning/CloningSystem.cs
@@ -164,13 +164,15 @@ public sealed partial class CloningSystem : EntitySystem
         return true;
     }
 
-    /// <summary>
-    ///     Copy components from one entity to another based on a CloningSettingsPrototype.
-    /// </summary>
-    /// <param name="original">The orignal Entity to clone components from.</param>
-    /// <param name="clone">The target Entity to clone components to.</param>
-    /// <param name="settings">The clone settings prototype containing the list of components to clone.</param>
-    public void CloneComponents(EntityUid original, EntityUid clone, CloningSettingsPrototype settings)
+    public override void CloneComponents(EntityUid original, EntityUid clone, ProtoId<CloningSettingsPrototype> settings)
+    {
+        if (!_prototype.Resolve(settings, out var proto))
+            return;
+
+        CloneComponents(original, clone, proto);
+    }
+
+    public override void CloneComponents(EntityUid original, EntityUid clone, CloningSettingsPrototype settings)
     {
         var componentsToCopy = settings.Components;
         var componentsToEvent = settings.EventComponents;
